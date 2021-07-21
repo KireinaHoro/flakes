384d08c9e13d313b6a8c57ed30019dc21561aac8
KireinaHoro <i@jsteward.moe>
2021-07-21 21:56:29 +0800

    refactor services section

--- nixos/minato/networking.nix
+++ nixos/minato/networking.nix
@@ -67,34 +67,36 @@ in
     };
   };
 
-  services.gravity = rec {
-    enable = true;
-    config = raitSecret;
-    netnsAddress = gravityAddr "2";
-    address = gravityAddr "1";
-    subnet = gravityAddr "";
-    group = 54;
-    inherit prefixLength;
-  };
+  services = {
+    gravity = rec {
+      enable = true;
+      config = raitSecret;
+      netnsAddress = gravityAddr "2";
+      address = gravityAddr "1";
+      subnet = gravityAddr "";
+      group = 54;
+      inherit prefixLength;
+    };
 
-  services.divi = {
-    enable = true;
-    prefix = "${iviDiviPrefix}4:0:4::/96";
-    address = "${iviDiviPrefix}4:0:5:0:3/128";
-    inherit ifName;
-  };
+    divi = {
+      enable = true;
+      prefix = "${iviDiviPrefix}4:0:4::/96";
+      address = "${iviDiviPrefix}4:0:5:0:3/128";
+      inherit ifName;
+    };
 
-  services.ivi = {
-    enable = true;
-    prefix4 = "10.172.208.0";
-    prefix6 = "2a0c:b641:69c:cd05:0:5";
-    defaultMap = "2a0c:b641:69c:f254:0:4::/96";
-    inherit prefixLength;
-  };
+    ivi = {
+      enable = true;
+      prefix4 = "10.172.208.0";
+      prefix6 = "2a0c:b641:69c:cd05:0:5";
+      defaultMap = "2a0c:b641:69c:f254:0:4::/96";
+      inherit prefixLength;
+    };
 
-  services.chinaLocalNat = {
-    enable = true;
-    inherit ifName;
-    prefix6 = "2a0c:b641:69c::/48";
+    chinaLocalNat = {
+      enable = true;
+      inherit ifName;
+      prefix6 = "2a0c:b641:69c::/48";
+    };
   };
 }
