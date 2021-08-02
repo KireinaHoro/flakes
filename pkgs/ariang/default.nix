84cf5e74fd4513d14ff56ebc9e9af47d07e6949b
KireinaHoro <i@jsteward.moe>
2021-08-02 12:25:03 +0800

    add ariang (wip)

---
+++ pkgs/ariang/default.nix
@@ -0,0 +1,18 @@
+{ source, pkgs, lib }: let
+  nodeDependencies = (pkgs.callPackage source.src {}).shell.nodeDependencies;
+in
+
+pkgs.stdenv.mkDerivation rec {
+  inherit (source) pname version src;
+
+  buildInputs = with pkgs; [ nodePackages.gulp ];
+  buildPhase = ''
+    ln -s ${nodeDependencies}/lib/node_modules ./node_modules
+    export PATH="${nodeDependencies}/bin:$PATH"
+    gulp clean build
+  '';
+  installPhase = ''
+    mkdir -p $out
+    mv dist $out
+  '';
+}
