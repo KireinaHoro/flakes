2854d57584ac037be6e3491952ea26dfce1f6c6e
KireinaHoro <i@jsteward.moe>
2021-09-23 15:25:32 +0200

    fix for unix domain socket forwarding

--- modules/common.nix
+++ modules/common.nix
@@ -10,7 +10,10 @@ self: { config, pkgs, lib, ... }:
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCoZdntY4uf9cnvIxy2aLE6URgzVTp5wLMBCU+HAq9e2t28GkgEl/QSr1K3aloGZ411YcbvQNv5yqx6tmrZjaOixefC6JKXU9agBNVZvhE6AUHNT3U97kmOiyEL3/HY2hGP9cR8sIfErI27+4a1W778CA6ARkBegvhWZ3NlwSol+RnR710x4lH6m7BsW2a9u/PC8JJPo2PJJrIjw7H8OcPUSZScE9/ztragghWhzdU+2Wsw/vwOdQDzg05518Xv+X92zc25F9CW/QD1f3DM0lonscljfHPvPazU7P7Px88o+W8d1Sik7bPwvct8Ce4iKcn0s4GNpbIoBVLdsrbdcrrM9Ge7ba/3UYRPkiTze3TLyrKJAAGtZ/lnXBBMhAzZxKVLairmhf/UZzaFYYTht7iDwXfAZCaZbz18yDOnj6zeGR97ZEhNlgFdF6z3AfYaTPLTYdKMUL41rpbj8Ipawg9tg7amcRZ/sdlepq6e47wUZv7f2h1z+xA4afSCMbDi/VjyIi918iymKLZX28CDvaAUJFoLus0JTenVGZ/xJx9Ngi+YOfBVnpP95xG9kW9kl0FR4KBdcOOC+folad69gFFOyi/GNaZO/k/zr+gf67P3TDm1yVkakF9Ey5qb8lD4ACtWTVIolUcrRhbYJJ9Su0uhS7wo+izXz7waAXHrRdyC5Q== cardno:000606940203"
     ];
   };
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    extraConfig = ''StreamLocalBindUnlink yes'';
+  };
 
   system.configurationRevision = if self ? rev then self.rev else "dirty";
 
