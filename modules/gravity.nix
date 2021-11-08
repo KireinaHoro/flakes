7fcc2429c0337245f5c440088e23869c4871d7ce
KireinaHoro <i@jsteward.moe>
2021-11-08 11:01:30 +0100

    Revert "extra check of dependency"
    This reverts commit 817d2bb66b38b459c72270a54ea546498f8eb014.

--- modules/gravity.nix
+++ modules/gravity.nix
@@ -124,10 +124,8 @@ in
         RestartSec = 5;
       };
       startLimitIntervalSec = 0;
-      wants = [ "network-online.target" "sys-devices-virtual-net-gravity.device" ];
-      after = [ "network-online.target" "sys-devices-virtual-net-gravity.device" ];
-      # only start after networkd setting up the veth, or the `ip link set netns` will fail
-      requires = [ "sys-devices-virtual-net-gravity.device" ];
+      wants = [ "network-online.target" ];
+      after = [ "network-online.target" ];
       wantedBy = [ "multi-user.target" ];
     };
   };
