7da59219044ded6175137d68ecff776bdfae6ced
KireinaHoro <i@jsteward.moe>
2022-05-13 10:57:03 +0200

    add webdav for zotero

--- nixos/nagisa/networking.nix
+++ nixos/nagisa/networking.nix
@@ -4,8 +4,8 @@ with pkgs.lib;
 
 let
   iviDiviPrefix = "2a0c:b641:69c:cf1";
-  gravityAddr = last: "${iviDiviPrefix}0::${last}/${toString prefixLength}";
-  raitSecret = config.sops.secrets.rait.path;
+  gravityAddrSingle = last: "${iviDiviPrefix}0::${last}";
+  gravityAddr = last: "${gravityAddrSingle last}/${toString prefixLength}";
   ifName = "enp0s3";
   prefixLength = 60;
   publicDNS = [ "2001:4860:4860::8888" "8.8.8.8" ];
@@ -41,14 +41,36 @@ in
 
     openssh.passwordAuthentication = false;
 
-    gravity = rec {
+    gravity = {
       enable = true;
-      config = raitSecret;
+      config = config.sops.secrets.rait.path;
       netnsAddress = gravityAddr "2";
       address = gravityAddr "1";
       subnet = gravityAddr "";
       inherit prefixLength;
       inherit gravityTable;
+    };
+
+    webdav = {
+      enable = true;
+      environmentFile = config.sops.secrets.webdav-env.path;
+      settings = {
+        address = "[${gravityAddrSingle "1"}]"; # only listen in gravity
+        port = 8080;
+        scope = "/srv/shared";
+        modify = false;
+        auth = true;
+        users = [
+          {
+            username = "{env}ZOT_USERNAME";
+            password = "{env}ZOT_PASSWORD";
+            rules = [
+              { allow = false; path = "/"; }
+              { allow = true; modify = true; path = "/zotero"; }
+            ];
+          }
+        ];
+      };
     };
   };
 }
