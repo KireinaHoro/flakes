7cb8e1d6d62556b4fb48421604d52bbfdb6015c7
KireinaHoro <i@jsteward.moe>
2022-12-28 07:09:12 +0800

    update to new nvfetcher

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -11,7 +11,7 @@ let
 in {
   packages = pkgs: mapPackages (name: pkgs.${name});
   overlay = final: prev: mapPackages (name: let
-    sources = import ./sources.nix { inherit (final) fetchurl fetchgit; };
+    sources = import ./sources.nix { inherit (final) fetchurl fetchgit fetchFromGitHub; };
     package = import (./. + "/${name}");
     args = intersectAttrs (functionArgs package) { source = sources.${name}; };
   in final.callPackage package args)
