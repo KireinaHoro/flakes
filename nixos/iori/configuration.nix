91f2dfedc694309d5cee195ca01e618a97836638
Pengcheng Xu <i@jsteward.moe>
2023-05-25 02:02:32 +0200

    gpg and ssh agent

--- nixos/iori/configuration.nix
+++ nixos/iori/configuration.nix
@@ -14,9 +14,35 @@
 
   i18n.defaultLocale = "en_US.UTF-8";
 
+  environment.systemPackages = with pkgs; [ xfsprogs stow gnupg lm_sensors ];
+
   programs = {
     mtr.enable = true;
     zsh.enable = true;
+    ssh.startAgent = false;
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+      pinentryFlavor = "curses";
+    };
   };
+
+  services.pcscd.enable = true;
+
+  # allow jsteward to use local smartcard over SSH
+  security.polkit.extraConfig = ''
+    /* allow jsteward to access PC/SC smartcards */
+    polkit.addRule(function(action, subject) {
+      if (action.id == "org.debian.pcsc-lite.access_pcsc" && subject.user == "jsteward") {
+        return polkit.Result.YES;
+      }
+    });
+
+    polkit.addRule(function(action, subject) {
+      if (action.id == "org.debian.pcsc-lite.access_card" && subject.user == "jsteward") {
+        return polkit.Result.YES;
+      }
+    });
+  '';
 }
 
