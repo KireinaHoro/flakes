0aeb1b8d306a8718cfa6f54e341994864e0af154
Pengcheng Xu <pengxu@ethz.ch>
2024-01-18 18:27:24 +0100

    move homebrew to darwin specific

--- darwin/HANA/home.nix
+++ darwin/HANA/home.nix
@@ -16,5 +16,18 @@ in
       ];
       packages = with pkgs; [ coreutils ];
     };
+
+    programs.zsh.initExtra = ''
+      # load homebrew
+      # FIXME: we should eventually completely switch to nix-darwin
+      if [ "$(arch)" = "arm64" ]; then
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+      else
+        eval "$(/usr/local/bin/brew shellenv)"
+      fi
+
+      # iTerm2 integration
+      test -e "$HOME/.iterm2_shell_integration.zsh" && source "$HOME/.iterm2_shell_integration.zsh"
+    '';
   };
 }
