ded9070c6916a4a98c70a5bcdc24659bef3f222a
Pengcheng Xu <pengxu@ethz.ch>
2024-10-24 16:26:56 +0200

    renovate gravity module to allow bird and ranet

--- modules/gravity.nix
+++ modules/gravity.nix
@@ -2,15 +2,20 @@
 with lib;
 let
   cfg = config.services.gravity;
+  raitConfigFile = config.sops.templates."rait.conf".path;
   ip = "${pkgs.iproute2}/bin/ip";
+  gravityPartDepend = after: {
+    partOf = [ "gravity.service" ];
+    after = [ "gravity.service" ] ++ after;
+    requires = [ "gravity.service" ];
+    requiredBy = [ "gravity.service" ];
+  };
+  gravityPart = gravityPartDepend [];
+  raitDeps = gravityPartDepend (optional cfg.babeld.enable "gravity-babeld.service");
 in
 {
   options.services.gravity = {
     enable = mkEnableOption "gravity overlay network";
-    config = mkOption {
-      type = types.path;
-      description = "path to rait config";
-    };
     address = mkOption {
       type = types.str;
       description = "address to add into main netns";
@@ -29,20 +34,10 @@ in
       description = "name of link connecting netns";
       default = "gravity";
     };
-    socket = mkOption {
-      type = types.str;
-      description = "path of babeld control socket";
-      default = "/run/babeld.ctl";
-    };
     defaultRoute = mkOption {
       type = types.bool;
       description = "enable default IPv6 route to gravity";
       default = false;
-    };
-    postStart = mkOption {
-      type = types.listOf types.str;
-      description = "additional commands to run after startup";
-      default = [ ];
     };
     prefixLength = mkOption {
       type = types.int;
@@ -70,12 +65,117 @@ in
       default = 3500;
     };
     extraRoutePolicies = mkOption {
+      type = types.listOf types.attrs;
       description = "extra systemd-network routing options to put on link";
       default = [];
     };
+
+    # routing daemon selection
+    babeld = mkOption {
+      type = types.submodule { options = {
+        enable = mkEnableOption "babeld for routing";
+        socket = mkOption {
+          type = types.str;
+          description = "path of babeld control socket";
+          default = "/run/babeld.ctl";
+        };
+      }; };
+      default = {};
+    };
+    bird = mkOption {
+      type = types.submodule { options = {
+        enable = mkEnableOption "bird for routing";
+      }; };
+      default = {};
+    };
+
+    # backbone selection
+    rait = mkOption {
+      type = types.submodule { options = {
+        enable = mkEnableOption "rait for WireGuard backbone";
+        secretNames = mkOption {
+          type = types.submodule { options = {
+            operatorKey = mkOption { type = types.str; default = "rait-operator-key"; };
+            nodeKey = mkOption { type = types.str; default = "rait-node-key"; };
+            registry = mkOption { type = types.str; default = "rait-registry"; };
+          }; };
+          default = {};
+        };
+        transports = mkOption {
+          type = types.listOf (types.submodule { options = {
+            family = mkOption { type = types.enum [ "ip4" "ip6" ]; };
+            address = mkOption { type = types.nullOr types.str; default = null; };
+            sendPort = mkOption { type = types.int; };
+            mtu = mkOption { type = types.int; };
+            randomPort = mkOption { type = types.bool; default = false; };
+          }; });
+        };
+      }; };
+      default = {};
+    };
+    ranet = mkOption {
+      type = types.submodule { options = {
+        enable = mkEnableOption "ranet for IPsec backbone";
+      }; };
+      default = {};
+    };
   };
+
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ wireguard-tools ];
+    assertions = [
+      { assertion = cfg.bird.enable || cfg.babeld.enable;
+        message = "at least one of bird and babeld should be enabled"; }
+      { assertion = cfg.rait.enable || cfg.ranet.enable;
+        message = "at least one of ranet (IPsec) and rait (WireGuard) should be enabled"; }
+      { assertion = cfg.rait.enable -> length cfg.rait.transports >= 1;
+        message = "must define at least one transport"; }
+
+      # TODO: implement these
+      { assertion = !cfg.ranet.enable;
+        message = "ranet (IPsec) not implemented yet"; }
+      { assertion = !cfg.bird.enable;
+        message = "bird not implemented yet"; }
+    ];
+
+    sops.templates."rait.conf".content = mkIf cfg.rait.enable ''
+      registry = "${config.sops.placeholder.${cfg.rait.secretNames.registry}}"
+      private_key = "${config.sops.placeholder.${cfg.rait.secretNames.nodeKey}}"
+      operator_key = "${config.sops.placeholder.${cfg.rait.secretNames.operatorKey}}"
+      namespace = "${cfg.netns}"
+
+
+      ${concatStringsSep "\n" (map (t: let
+        familyDigit = substring 2 1 t.family;
+        mark = "5${familyDigit}";
+      in ''
+        transport {
+          address_family = "${t.family}"
+          ${optionalString (t.address != null) "address = \"${t.address}\""}
+          send_port = ${toString t.sendPort}
+          mtu = ${toString t.mtu}
+          ifprefix = "grv${familyDigit}x"
+          ifgroup = ${mark}
+          fwmark = ${mark}
+          random_port = ${boolToString t.randomPort}
+        }
+        '') cfg.rait.transports)}
+
+        ${optionalString cfg.babeld.enable ''
+          babeld {
+            enabled = true
+            socket_type = "unix"
+            socket_addr = "${cfg.babeld.socket}"
+            footnote = "interface host type wired"
+          }
+        ''}
+
+        remarks = {
+          name = "${config.networking.hostName}"
+          prefix = "${cfg.subnet}"
+        }
+    '';
+
+    environment.systemPackages = mkIf cfg.rait.enable (with pkgs; [ wireguard-tools ]);
 
     systemd.network = {
       netdevs = pkgs.injectNetdevNames {
@@ -102,26 +202,55 @@ in
       };
     };
 
-    systemd.timers."gravity-sync" = {
+    systemd.timers.gravity-rait-sync = mkIf cfg.rait.enable ({
       wantedBy = [ "timers.target" ];
       timerConfig = {
         OnBootSec = "15m";
         OnUnitActiveSec = "15m";
-        Unit = "gravity-sync.service";
+        Unit = "gravity-rait-sync.service";
       };
-    };
-    systemd.services."gravity-sync" = {
+    } // raitDeps);
+    systemd.services.gravity-rait-sync = mkIf cfg.rait.enable ({
       serviceConfig = with pkgs; {
         Type = "oneshot";
         User = "root";
-        ExecStart = "${rait}/bin/rait sync -c ${cfg.config}";
+        ExecStart = "${rait}/bin/rait sync -c ${raitConfigFile}";
       };
-    };
+    } // raitDeps);
+    systemd.services.gravity-rait-up = mkIf cfg.rait.enable ({
+      serviceConfig = with pkgs; {
+        Type = "oneshot";
+        User = "root";
+        ExecStart = [ "${rait}/bin/rait up -c ${raitConfigFile}" ];
+      };
+    } // raitDeps);
+
+    systemd.services.gravity-babeld = mkIf cfg.babeld.enable ({
+      serviceConfig = with pkgs; {
+        ExecStart = "${ip} netns exec ${cfg.netns} ${babeld}/bin/babeld -c ${writeText "babeld.conf" ''
+          random-id true
+          local-path-readwrite ${cfg.babeld.socket}
+          state-file ""
+          pid-file ""
+          interface placeholder
+
+          redistribute local deny
+          redistribute ip ${cfg.route} eq ${toString cfg.prefixLength} allow
+        ''}";
+        Restart = "always";
+        RestartSec = 5;
+      };
+    } // gravityPart);
 
     systemd.services.gravity = {
       serviceConfig = with pkgs; {
-        ExecStartPre = [
+        # set up netns and veth pair and invoke rait (if enabled)
+        # the routing daemons are separate services
+        Type = "oneshot";
+        RemainAfterExit = true;
+        ExecStart = [
           # FIXME move to networkd when netns support lands there
+          #       https://github.com/systemd/systemd/issues/11103
           "networkctl reload"
           "-${ip} netns add ${cfg.netns}" # do not panic when a stale netns was not deleted
           "${ip} link set host netns ${cfg.netns}"
@@ -132,27 +261,11 @@ in
           "${ip} -n ${cfg.netns} route add ${cfg.subnet} via fe80::200:ff:fe00:1 dev host metric 1 proto static"
           "${ip} -n ${cfg.netns} addr add ${cfg.subnet} dev lo"
         ];
-        ExecStart = "${ip} netns exec ${cfg.netns} ${babeld}/bin/babeld -c ${writeText "babeld.conf" ''
-          random-id true
-          local-path-readwrite ${cfg.socket}
-          state-file ""
-          pid-file ""
-          interface placeholder
-
-          redistribute local deny
-          redistribute ip ${cfg.route} eq ${toString cfg.prefixLength} allow
-        ''}";
-        ExecStartPost = [
-          "${rait}/bin/rait up -c ${cfg.config}"
-        ] ++ cfg.postStart;
-        ExecReload = "${rait}/bin/rait sync -c ${cfg.config}";
-        ExecStopPost = [
+        ExecStop = [
           # restore host back to default namespace, or it will be deleted along with the netns
           "${ip} -n ${cfg.netns} link set host netns 1"
           "${ip} netns del ${cfg.netns}"
         ];
-        Restart = "always";
-        RestartSec = 5;
       };
       startLimitIntervalSec = 0;
       wants = [ "network-online.target" ];
