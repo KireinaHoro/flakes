4516d63295aa0d6a100c8b4d8521c7be87d561e2
Pengcheng Xu <pengxu@ethz.ch>
2024-10-24 16:49:27 +0200

    toride does not have gravity deployed

--- nixos/toride/networking.nix
+++ nixos/toride/networking.nix
@@ -4,8 +4,6 @@ with pkgs.lib;
 
 let
   ifName = "eth0";
-  gravityTable = 3500;
-  gravityMark = 333;
 in
 
 {
@@ -16,23 +14,6 @@ in
     hostName = "toride";
     useDHCP = false;
     firewall.enable = false;
-  };
-
-  # FIXME merge masquerade into networkd configuration
-  networking.nftables = {
-    ruleset = ''
-      table inet local-wan {
-        chain filter {
-          type filter hook forward priority 100;
-          oifname "${ifName}" ip saddr != { 10.160.0.0/12, 10.208.0.0/12 } log prefix "Unknown source to WAN: " drop
-          oifname "${ifName}" ip6 saddr != ${iviDiviPrefix}0::/${toString prefixLength} log prefix "Unknown source to WAN: " drop
-        }
-        chain nat {
-          type nat hook postrouting priority 100;
-          oifname "${ifName}" masquerade;
-        }
-      }
-    '';
   };
 
   systemd.network = {
@@ -48,15 +29,5 @@ in
     vnstat = { enable = true; };
 
     openssh.settings.PasswordAuthentication = false;
-
-#    gravity = rec {
-#      enable = true;
-#      config = raitSecret;
-#      netnsAddress = gravityAddr "2";
-#      address = gravityAddr "1";
-#      subnet = gravityAddr "";
-#      inherit prefixLength;
-#      inherit gravityTable;
-#    };
   };
 }
