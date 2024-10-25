4759d1d328e1c86def50a0ec81bedaa3b443af23
Pengcheng Xu <i@jsteward.moe>
2024-10-25 19:24:27 +0200

    use uuid to mount disks

--- nixos/shigeru/hardware.nix
+++ nixos/shigeru/hardware.nix
@@ -11,5 +11,6 @@
   boot.initrd.kernelModules = [ "nvme" ];
 
   fileSystems."/boot" = { device = "/dev/disk/by-uuid/6A41-3ED8"; fsType = "vfat"; };
-  fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
+  fileSystems."/" = { device = "/dev/disk/by-uuid/25c29fa5-df2a-437f-a05d-c1c3b61a8729"; fsType = "ext4"; };
+  swapDevices = [ { device = "/dev/disk/by-uuid/00fb9e9a-5251-431f-8b8e-9b2bdcb8e5e5"; } ];
 }
