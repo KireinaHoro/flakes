ca819e2fec5cbd2364e7fac9502070241f403852
Pengcheng Xu <i@jsteward.moe>
2024-10-25 19:32:01 +0200

    use bigger partition as boot

--- nixos/shigeru/hardware.nix
+++ nixos/shigeru/hardware.nix
@@ -10,7 +10,6 @@
   };
   boot.initrd.kernelModules = [ "nvme" ];
 
-  fileSystems."/boot" = { device = "/dev/disk/by-uuid/6A41-3ED8"; fsType = "vfat"; };
+  fileSystems."/boot" = { device = "/dev/disk/by-uuid/5D86-06F8"; fsType = "vfat"; };
   fileSystems."/" = { device = "/dev/disk/by-uuid/25c29fa5-df2a-437f-a05d-c1c3b61a8729"; fsType = "ext4"; };
-  swapDevices = [ { device = "/dev/disk/by-uuid/00fb9e9a-5251-431f-8b8e-9b2bdcb8e5e5"; } ];
 }
