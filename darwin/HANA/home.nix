a6baa844077a4e07891347dee1fca4beb2745e93
KireinaHoro <i@jsteward.moe>
2026-01-14 11:07:34 +0100

    homebrew is now gone

--- darwin/HANA/home.nix
+++ darwin/HANA/home.nix
@@ -4,28 +4,14 @@
   imports = [ ../../hm-modules/home-common.nix ];
 
   home = {
-    sessionPath = [
-      "${homeDir}/sdk/go1.20.3/bin"
-      "${homeDir}/go/bin"
-    ];
     packages = with pkgs; [
-      coreutils clang
+      coreutils clang git
       texlive.combined.scheme-full
     ];
   };
 
-  programs.zsh.initExtra = ''
-    # load homebrew
-    # FIXME: we should eventually completely switch to nix-darwin
-    if [ "$(arch)" = "arm64" ]; then
-      eval "$(/opt/homebrew/bin/brew shellenv)"
-    else
-      eval "$(/usr/local/bin/brew shellenv)"
-    fi
-
-    # remove walters RPS1
-    unset RPS1
-  '';
+  # disable GUI for vim
+  programs.vim.packageConfigurable = pkgs.vim;
 
   programs.neovim.enable = true;
 }
