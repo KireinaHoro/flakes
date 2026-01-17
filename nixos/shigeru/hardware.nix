ec914ffda6c6d5e45ad4278b9c9661160092e5fb
Pengcheng Xu <i@jsteward.moe>
2026-01-17 20:45:45 +0100

    use supported efi mount location

--- nixos/shigeru/hardware.nix
+++ nixos/shigeru/hardware.nix
@@ -9,13 +9,13 @@
       device = "nodev";
     };
     efi = {
-      efiSysMountPoint = "/efi";
+      efiSysMountPoint = "/boot/efi";
       canTouchEfiVariables = true;
     };
   };
   boot.initrd.kernelModules = [ "nvme" ];
 
-  fileSystems."/efi" = { device = "/dev/disk/by-uuid/6A41-3ED8"; fsType = "vfat"; };
+  fileSystems."/boot/efi" = { device = "/dev/disk/by-uuid/6A41-3ED8"; fsType = "vfat"; };
   fileSystems."/" = { device = "/dev/disk/by-uuid/25c29fa5-df2a-437f-a05d-c1c3b61a8729"; fsType = "ext4"; };
 
   swapDevices = [ { device = "/dev/disk/by-uuid/068dda56-7795-46ef-b21a-0c18037c8acb"; } ];
