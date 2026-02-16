794d2e46565def7f88dd297e0d1553ab570a85ce
KireinaHoro <i@jsteward.moe>
2026-02-16 19:12:25 +0800

    tool to clean up mess

--- darwin/HANA/home.nix
+++ darwin/HANA/home.nix
@@ -5,7 +5,9 @@
 
   home = {
     packages = with pkgs; [
-      coreutils clang git git-lfs gtkwave python3
+      coreutils clang
+      git git-lfs git-filter-repo
+      gtkwave python3
       texlive.combined.scheme-full
       yubikey-manager smartmontools baobab
       imagemagick htop
